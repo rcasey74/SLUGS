99ce494c1f01441cf4b5c73abf388752696acb98
Mariano Lizarraga <malife@gmail.com>
2011-01-04 11:36:31 -0600

    Created testSuite in Code::Blocks using the Catch test framework

--- clib/testSuite/main.cpp
+++ clib/testSuite/main.cpp
@@ -1,22 +1,27 @@
 #include "catch_default_main.hpp" // This brings in a default implementation for main()
-#include "../circBuffer.h"
+#include "circBuffer.h"
 
 
   TEST_CASE( "Anadir y quitar elementos", "La cola debe de anadir y remover elementos de manera adecuada" )
   {
-  		CBRef bp = newCircBuffer(10);
+      struct CircBuffer buffer;
+
+  		CBRef bp = &buffer;
+  		newCircBuffer(bp);
+
   		int x;
-  	
+
+
   		// Anadir un elemento
-	    writeBack(bp, 5);  		
+	    writeBack(bp, 5);
   		REQUIRE( peak(bp) == 5 );
       REQUIRE( getLength(bp) == 1 );
-      
-      
+
+
       // Quitar un elemento
 	    x = readHead(bp);
       REQUIRE( x == 5 );
       REQUIRE( getLength(bp) == 0 );
-      
-      freeCircBuffer(bp)
+
+//      freeCircBuffer(bp)
   }
