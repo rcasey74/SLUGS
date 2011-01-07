2e575e8f2a8e32c68cff3076e59c63a92b0f2105
Alberto Ferrer <albertoeferrer@gmail.com>
2011-01-07 12:27:09 -0600

    test de circbufer

--- clib/circBuffer.c
+++ clib/circBuffer.c
@@ -27,14 +27,14 @@ THE SOFTWARE.
 // circBuffer.c
 // This is the implementation file for the circular buffer. This library
 // implements a circular buffer to be used in the serial readers
-// for the GPS, IPC and telemetry decoders of the UCSC AutoPilot. 
+// for the GPS, IPC and telemetry decoders of the UCSC AutoPilot.
 //
 // Comment the DEBUG define int the header prior to compile for dsPIC
 //
 // Convemtion Notes
 // The HEAD, as in any queue points to the next available byte to be READ
 // The TAIL, points to the next available position to be written
-// 
+//
 // Code by: Mariano I. Lizarraga
 // First Revision: Aug 16 2008 @ 00:36
 // Second Revision: Dec 2 2008 @ 12:11
@@ -51,17 +51,17 @@ THE SOFTWARE.
 
 // Constructors - Destructors
 // ==========================
-// this Function returns a pointer to a new Circular Buffer of size pm_size 
+// this Function returns a pointer to a new Circular Buffer of size pm_size
 
 #if __IN_DSPIC__
 	void newCircBuffer (CBRef cB){
-		
+
 		// initialize to zero
 		int i;
 		for (i=0; i<BSIZE; i++){
 			cB->buffer[i] = 0;
 		}
-				
+
 		// initialize the data members
 		cB->head = 0;
 		cB->tail = 0;
@@ -70,11 +70,11 @@ THE SOFTWARE.
 
 	}
 
-// this function frees the Circular Buffer CB Ref
+//this function frees the Circular Buffer CB Ref
 	void freeCircBuffer (CBRef* cB){
 		// if it is already null, nothing to free
 		if (cB == NULL || *cB == NULL) {return;}
-				
+
 		// free and nil the pointer
 		//free(*cB);
 		*cB = NULL;
@@ -85,60 +85,60 @@ THE SOFTWARE.
 	CBRef newCircBuffer (int pm_size){
 		// create the circular buffer pointer
 		CBRef cB;
-		
+
 		// allocate memory for it
 		cB = (CBRef) malloc(sizeof(CircBuffer));
-		
+
 		// allocate memory for the buffer
 		cB->buffer = (unsigned char *)calloc(pm_size, sizeof(unsigned char));
-		
+
 		// initialize the data members
 		cB->head = 0;
 		cB->tail = 0;
 		cB->size = pm_size;
 		cB->overflowCount = 0;
-		
+
 		// return the buffer pointer
 		return (cB);
-		
+
 	}
 
-  
+
 
 	// this function frees the Circular Buffer CB Ref
 	void freeCircBuffer (CBRef* cB){
 		// if it is already null, nothing to free
 		if (cB == NULL || *cB == NULL) {return;}
-		
+
 		// free the buffer
 		free((*cB)->buffer);
-		
+
 		// free and nil the pointer
 		free(*cB);
 		*cB = NULL;
 	}
 
 #endif
-	
+
 
 
 // Accesor Methods
 // ===============
 
 // returns the amount of unread bytes in the circular buffer
-unsigned int getLength (CBRef cB){	
+unsigned int getLength (CBRef cB){
 	// if the circular buffer is not null
 	if (cB != NULL){
 		if (cB->head <= cB->tail){
 			return (cB->tail-cB->head);
 		} else{
 			return (cB->size + cB->tail - cB->head);
-		}		
+		}
 	}
 	else{
 		return 0;
 	}
-	
+
 
 }
 
@@ -172,13 +172,13 @@ int readTail (CBRef cB){
 unsigned char peak(CBRef cB){
 	// if the circular buffer is not null
 	if (cB != NULL)
-	{	
+	{
 		// if there are bytes in the buffer
 		if (getLength(cB) > 0){
 			return cB->buffer[cB->head];
 		}
 	}
-	return 0;	
+	return 0;
 }
 
 
@@ -188,7 +188,7 @@ unsigned char peak(CBRef cB){
 unsigned char readFront (CBRef cB){
 	// if the circular buffer is not null
 	if (cB != NULL)
-	{	
+	{
 		char retVal;
 		// if there are bytes in the buffer
 		if (getLength(cB) > 0){
@@ -201,15 +201,15 @@ unsigned char readFront (CBRef cB){
 	return 254;
 }
 
-// writes one byte at the end of the circular buffer, 
+// writes one byte at the end of the circular buffer,
 // increments overflow count if overflow occurs
 unsigned char writeBack (CBRef cB, unsigned char data){
 	// if the circular buffer is not null
-	if (cB != NULL){			
+	if (cB != NULL){
 		if (getLength (cB) == (cB->size -1)){
 			cB->overflowCount ++;
 			//return 1;
-		} else {		
+		} else {
 			cB->buffer[cB->tail] = data;
 			cB->tail = cB->tail < (cB->size -1)? cB->tail+1: 0;
 			//return 0;
